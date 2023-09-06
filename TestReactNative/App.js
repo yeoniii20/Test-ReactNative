3973d700fdd3b0daa761809e1e31f27244ef699f
KwonSuyeon <commetoi_yeoni@naver.com>
2023-09-07 01:23:41 +0900

    fix: 페이지 및 컴포넌트 확인

---
+++ TestReactNative/App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import Login from "./constant/Login";
+import SiginUp from "./constant/SiginUp";
+import Statistics from "./Page/statistics";
+import Note from "./Page/note";
+import Bottom from "./components/Bottom";
+import Top from "./components/Top";
+import Bar from "./components/Bar";
+import Message from "./components/Modal/message";
+import FriendsList from "./Page/friendsList";
+import TestPage from "./Page/testPage";
+
+export default function App() {
+  return (
+    <View>
+      {/* <Login /> */}
+      {/* <SiginUp /> */}
+      {/* <Statistics /> */}
+      {/* <Note /> */}
+      {/* <Bottom /> */}
+      {/* <Top /> */}
+      {/* <Bar /> */}
+      {/* <Message /> */}
+      {/* <FriendsList /> */}
+      <TestPage />
+    </View>
+  );
+}
